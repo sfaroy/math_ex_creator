4a4f388a247c67008537f9ab3366e510199c979f
roee <sfa.roy@gmail.com>
2018-02-26 21:03:28 +0200

    distribution

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
-from distutils.core import setup
-import py2exe
+#from distutils.core import setup
+#import py2exe
 
-setup(console=['exercise_creator.py'])
+#setup(console=['exercise_creator.py'])
+
+
+import py2exe
+from distutils.core import setup
+setup( console=[{"script": "exercise_creator.py"}] )
